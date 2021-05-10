84d5181665768f8dd4e390b6a5bfa6d5ecb81da6
road <road@DESKTOP-7LT986R>
2021-05-10 10:17:01 +0900

    GGY 0510 test1

--- SST/src/main/java/org/sst/controller/HomeController.java
+++ SST/src/main/java/org/sst/controller/HomeController.java
@@ -34,6 +34,8 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+		
+		System.out.println("길경윤");
 	}
 	
 }
---
+++ SST/src/main/java/org/sst/controller/TestController.java
@@ -0,0 +1,6 @@
+package org.sst.controller;
+
+public class TestController {
+	
+
+}
