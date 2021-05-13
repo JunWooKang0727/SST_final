ae003a2fac4d50ec356239d8a31e76a899599fd9
imhyyoo <imhyyoo@gmail.com>
2021-05-13 10:47:19 +0900

    [0513 10:47] member, group 커밋

---
+++ SST/src/test/java/org/sst/mapper/MemberMapperTests.java
@@ -0,0 +1,66 @@
+package org.sst.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.sst.domain.MemberVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MemberMapperTests {
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper mapper;
+	
+	// 회원가입
+	@Test
+	public void testMemberInsert(){
+		MemberVO mem = new MemberVO();
+		mem.setM_id("test0620");
+		mem.setM_pw("test0620");
+		mem.setM_phone("test0620");
+		mem.setM_email("test0620");
+		mem.setM_birth("test0620");
+		mem.setM_name("test0620");
+		mapper.memberInsert(mem);
+		log.info(mem);
+	}
+	
+	// 회원정보 조회
+	@Test
+	public void testMemberRead(){
+		MemberVO mem = mapper.memberRead("test0620");
+		log.info(mem);
+	}
+	
+	// 회원정보 수정
+	@Test
+	public void testMemberModify(){
+		MemberVO mem = mapper.memberRead("test0620");
+		mem.setM_phone("11111");
+		mapper.memberUpdate(mem);
+	}
+
+	// 회원탈퇴
+	@Test
+	public void testMemberDelete(){
+		MemberVO mem = new MemberVO();
+		mem.setM_id("test0620");
+		mem.setM_pw("test0510");
+		mapper.memberDelete(mem);
+	}
+	
+	// 아이디 중복 확인
+	
+	@Test
+	public void testMemberIdCheck(){
+		int count = mapper.memberIdCheck("member01");
+		log.info(count);
+	}
+	
+}
