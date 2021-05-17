e29b5504d9e73bc656ba3a71971648b2621d64c6
seongmin an <cleric25@daum.net>
2021-05-17 17:50:55 +0900

    매개변수로 변환중 ver1.0

--- SST/src/main/java/org/sst/domain/Crawler.java
+++ SST/src/main/java/org/sst/domain/Crawler.java
@@ -1,29 +1,42 @@
 package org.sst.domain;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Crawler {
-	public static void getEx(String exUrl) throws IOException{
-		Connection.Response response = Jsoup.connect("exUrl")
-                .method(Connection.Method.GET)
-                .execute();
-		Document googleDocument = response.parse();
-		Element btnK = googleDocument.getElementsByTag("cr-icon-button").first();
-		String btnKValue = btnK.attr("title");
-
-		System.out.println(btnKValue); 
-
+	static String exUrl = "https://wdown.ebsi.co.kr/W61001/01exam";
+	
+	public static void getEx(String downP,String downH) throws IOException{
+		int fileNameIndex = exUrl.length();
+		System.out.println(downP.substring(fileNameIndex+14)+"문제파일이름입니다.");
+		InputStream in = new URL(downP).openStream();
+		//파일 카피해버리기.
+		Files.copy(in, Paths.get("C:/upload/new/"+downP.substring(fileNameIndex+14)), StandardCopyOption.REPLACE_EXISTING);
+		in.close();
+		System.out.println(downH.substring(fileNameIndex+14)+"해설파일이름입니다.");
+		InputStream in2 = new URL(downH).openStream();
+		//파일 카피해버리기.
+		Files.copy(in2, Paths.get("C:/upload/new/"+downH.substring(fileNameIndex+14)), StandardCopyOption.REPLACE_EXISTING);
+		in2.close();
+		
+		System.out.println("성공입니다");
 	}
-	public static void main(String[] args) throws IOException {
+	
+	public Object[] urlFromEbs() throws IOException {
 		String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36";
 		Document doc = Jsoup.connect("https://www.ebsi.co.kr/ebs/xip/xipc/previousPaperListAjax.ajax")
 				.method(Connection.Method.GET)
@@ -34,27 +47,11 @@ public class Crawler {
 				.header("Accept-Encoding", "gzip, deflate, br") 
 				.header("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
 				.data("targetCd", "D300")//이아래부터 우리가 만져줘야함
-				.data("monthList", "03,04,05,06,07,09,10,11,12")
-				.data("subjList", "1,6,7,8")
+				.data("monthList", "03")//여기에 월 추가
+				.data("subjList", "1")//국어1 수학2 영어3 한국사4 사회탐구5 과학탐구6 직업탐구7 제2외국어8
 				.data("sort", "recent")
 				.data("beginYear", "2018")
 				.data("endYear", "2021")
-				.data("monthAll", "")
-				.data("month", "on")
-				.data("month", "03")
-				.data("month", "04")
-				.data("month", "05")
-				.data("month", "06")
-				.data("month", "07")
-				.data("month", "09")
-				.data("month", "10")
-				.data("month", "11")
-				.data("month", "12")
-				.data("subjAll", "on")
-				.data("subjAll", "1")
-				.data("subjAll", "6")
-				.data("subjAll", "7")
-				.data("subjAll", "8")
 				.ignoreContentType(false)
 				.get();
 		
@@ -66,7 +63,7 @@ public class Crawler {
 		}
 		ArrayList<String> downP = new ArrayList<String>();
 		ArrayList<String> downH = new ArrayList<String>();
-		String exUrl = "https://wdown.ebsi.co.kr/W61001/01exam";
+		
 		for(int i = 0 ; i<buttons.size();i++){
 			if(buttons.get(i).contains("goDownLoadP")){
 				String[] downPSp = null;
@@ -80,10 +77,17 @@ public class Crawler {
 			
 		}
 		for(int i = 0 ; i<downH.size();i++){
-			System.out.println(downP.get(i));
-			System.out.println(downH.get(i));
+			System.out.println(downP.get(i)+"문제 경로입니다");
+			System.out.println(downH.get(i)+"해설 경로입니다");
 		}
-		
-	}//main end
-
+		/*for(int i = 0 ; i<downH.size();i++){
+			getEx(downP.get(i));
+		}*/
+		Object[] exMak = new Object[2];
+		exMak[0] = downP;
+		exMak[1] = downH;
+		return exMak;
+	}
 }
+
+
--- SST/src/main/java/org/sst/domain/pdftest.java
+++ SST/src/main/java/org/sst/domain/pdftest.java
@@ -1,154 +1,52 @@
 package org.sst.domain;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import com.itextpdf.text.Chunk;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
+
 
 public class pdftest {
-	public static void pdfCreate(ArrayList<String> exRealMultiList,ArrayList<String> exRealTextList) throws Exception {
-	    String fileName="";
-	    String dir="C:/Users/ki11e/upload";
-	    fileName = "simple_table3.pdf";
-	    BaseFont baseFont = BaseFont.createFont("C:/Windows/Fonts/malgun.ttf", BaseFont.IDENTITY_H,
-                BaseFont.EMBEDDED);
 
-	    Font font = new Font(baseFont, 12);
-	    
-	    File directory = new File(dir);
-	    if(!directory.exists()) directory.mkdirs(); //파일경로 없으면 생성
-	     
-	     Document document = new Document();
-	     PdfWriter.getInstance(document, new FileOutputStream(dir+"/"+fileName));
-	         
-	      document.open();
-	      
-	 
-	        for(int i = 0; i < exRealMultiList.size(); i++){
-	        	PdfPTable pdfTable = new PdfPTable(1);
-	        	Chunk chunk = new Chunk(exRealMultiList.get(i), font);
-	        	Chunk chunk2 = new Chunk(exRealTextList.get(i), font);
-	    	    Paragraph ph = new Paragraph(chunk);
-	    	    Paragraph ph2 = new Paragraph(chunk2);
-	        	pdfTable.addCell(ph);
-	        	pdfTable.addCell(ph2);
-	        	document.add(pdfTable);
-	        	System.out.println("완료입니다.");
-	        }
-	        document.close();
-	  }
 	public static void main(String[] args) throws Exception {
-
-		ArrayList<String> exNumList = new ArrayList<String>();
-		ArrayList<String> exMultiStartNumList = new ArrayList<String>();
-		ArrayList<String> exMultiEndNumList = new ArrayList<String>();
+		Crawler cr = new Crawler();
+		pdftestM pm = new pdftestM();
+		int urlLeng = cr.exUrl.length();
+		System.out.println("1."+urlLeng+"파일이 저장될 경로입니다.");
+		Object[] ur = cr.urlFromEbs();
+		System.out.println("EBS사이트에서 경로를 받아왔습니다.");
+		ArrayList<String> downPList  = (ArrayList<String>)ur[0];
+		System.out.println(downPList.size()+"가지고있는 url개수");
+		System.out.println("문제 URL리스트입니다.");
+		ArrayList<String> downHList  = (ArrayList<String>)ur[1];
+		System.out.println("해설 URL리스트입니다.");
 		
-		ArrayList<String> exMultiList = new ArrayList<String>();
-		ArrayList<String> exTextList = new ArrayList<String>();
-		ArrayList<String> exRealMultiList = new ArrayList<String>();
-		ArrayList<String> exRealTextList = new ArrayList<String>();
-		/*System.out.println("=============================================================");*/
-		String fileName2 = "C:/Users/ki11e/upload/example/korB_hsj_V4AQ713J.pdf";
-		File source2 = new File(fileName2);
-		PDDocument pdfDoc2 = PDDocument.load(source2);
-		String text2 = new PDFTextStripper().getText(pdfDoc2);
 		
-		String ahn = text2;
-
-		String reg = "(\\[출제의도\\]){1}\\p{Space}*(매체)";
-
-		String[] result = ahn.split(reg);
-
-		for (int i = 0; i < result.length; i++) {
-			/*System.out.println("====");
-			System.out.println(result[i]);*/
-			if (i > 0) {
-				String num = result[i - 1].substring(result[i - 1].length() - 4, result[i - 1].length() - 2);
-				exNumList.add(num);
-				/*System.out.println(result[i - 1].substring(result[i - 1].length() - 4).length() + "길이이이이이ㅣㅇ");
-				System.out.println("문제번호입니다 :" + num);*/
+		for(int i = 0 ; i<downPList.size();i++){
+			try {
+				cr.getEx(downPList.get(i), downHList.get(i));
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			/*System.out.println("====");
-			System.out.println(result.length);*/
-		}
-		String fileName = "C:/Users/ki11e/upload/example/korB_mun_8S782INA.pdf";
-		File source = new File(fileName);
-		PDDocument pdfDoc = PDDocument.load(source);
-		String text = new PDFTextStripper().getText(pdfDoc);
-		Pattern pattern = Pattern.compile("\\[{1}\\d{1,2}(\\p{Space}~\\p{Space}){1}\\d{1,2}\\]{1}");
-		Matcher matcher = pattern.matcher(text);
-		int count = 0;
-		while (matcher.find()) {
-			count++;
-			System.out.println("Match number " + count);
-			System.out.println("group(): " + matcher.group());
-			//다중지문 시작 인덱스
-			System.out.println("start(): " + matcher.start());
-		
-			System.out.println("end(): " + matcher.end());
-			String trans = matcher.group();
-			/* trans.replace("]", "").replace(" ","").split("~"); */
-			String BefSplit = trans.replace("[", "").replace("]", "").replace(" ", "");
-			String[] exRange = BefSplit.split("~");
-			/*
-			 * for(int i = 0 ; i<arr2.length;i++){ System.out.println(arr2[i]);
-			 * }
-			 */
-			System.out.println(text.indexOf(exRange[0]+".")+"지문시작 부터 지문까지");
-			exMultiStartNumList.add(exRange[0]);
-			exMultiEndNumList.add(exRange[1]);
 			
-			int seSNum = Integer.parseInt(exRange[0]);
-			int seENum = Integer.parseInt(exRange[1]);
-			for (int i = seSNum; i <= seENum; i++) {
-				for (int j = 0; j < exNumList.size(); j++) {
-					System.out.println("도는거"+i+" : "+j);
-					if (i == Integer.parseInt(exNumList.get(j))) {
-						System.out.println("일치하는거"+i+" : "+j);
-						exMultiList.add(text.substring(matcher.start() , text.indexOf(exRange[0]+".")-1));
-						break;
-					}
+			System.out.println("문제:"+downPList.get(i)+"\nURL을 통해 각 지정된 경로에 문제를 넣습니다.");
+			System.out.println("해설:"+downHList.get(i)+"\nURL을 통해 각 지정된 경로에 해설을 넣습니다.");
+			System.out.println(i+"번째입니다.");
+			Object[] r = pm.findEx(
+					downPList.get(i).substring(urlLeng+14),
+					"C:/upload/new/",
+					downHList.get(i).substring(urlLeng+14),
+					"표현상 특징을 파악한다");
+			
+				System.out.println("순서대로 (문제, 경로, 해설, 찾을 검색어) 를 넣습니다. 그러면 OBJCET LIST를 반환하는데 이둘은 각각 문제를만듭니다.");
+				try {
+					pm.pdfCreate((ArrayList<String>) r[0], (ArrayList<String>) r[1],"sample");
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			}
-			System.out.println("멍청한녀석"+exMultiList.size());
-		}
-		for (int i = 0; i < exNumList.size(); i++) {
-			if (!exNumList.get(i).equals("45")) {
-				//문제번호 = exNumList.get(i)".");
-				int exNumBefore = text.indexOf(exNumList.get(i) + ".");
-				//문제번호 인덱스 = exNumBefore);
-				//문제 다음번호 Integer.parseInt(exNumList.get(i)) + 1) + '.'
-				//문제 다음번호 인덱스 exNumAfter
-				int exNumAfter = text.indexOf((Integer.parseInt(exNumList.get(i)) + 1) + ".");
-				/*System.out.println("문제다음번호 인덱스다" + exNumAfter);
-				System.out.println("===========문제===========");*/
-				exTextList.add(text.substring(exNumBefore, exNumAfter));
-				/*System.out.println(text.substring(exNumBefore, exNumAfter));
-				System.out.println("===========문제끝===========");*/
-			} else {
-				int exNumBefore = text.indexOf(exNumList.get(i) + ".");
-				exTextList.add(text.substring(exNumBefore, text.length()));
-				/*System.out.println("문제번호다" + exNumList.get(i) + ".");*/
-			}
-		}
 
-		for(int i = 0 ; i<exTextList.size();i++){
-			exRealMultiList.add("연관된 지문: \n"+exMultiList.get(i));
-			exRealTextList.add("찾으시는 문제:\n"+exTextList.get(i));
+			
 			
 		}
-		System.out.println("바보녀석!"+exTextList.size());
-		pdfCreate(exRealMultiList, exRealTextList);
+		
+		
 	}
-	
 }
